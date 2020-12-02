7ed95986080386f6fcbfb6afa8bb59ec8980500b
nerichardson <nerichardson@expedia.com>
2020-12-01 20:28:57 -0600

    NealSr - formatting

--- day-1/day-1.rb
+++ day-1/day-1.rb
@@ -2,13 +2,13 @@ numbers = File.readlines('input.txt')
 
 for num1 in 0..numbers.length do
   for num2 in 1..numbers.length do
-  	for num3 in 2..numbers.length do
+    for num3 in 2..numbers.length do
       sum = numbers[num1].to_i + numbers[num2].to_i + numbers[num3].to_i
-  	  if sum == 2020 then
-  		puts numbers[num1]
-  		puts numbers[num2]
-  		puts numbers[num3]
-  	  end
-  	end
+      if sum == 2020 then
+        puts numbers[num1]
+        puts numbers[num2]
+        puts numbers[num3]
+      end
+    end
   end
 end
