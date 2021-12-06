832fdd1e885a3e260c611a67d3cdf2b3b4217dfe
nerichardson <nerichardson@expedia.com>
2021-12-06 09:06:52 -0600

    NealSr - simplify

--- 2021/day-06/day-06.rb
+++ 2021/day-06/day-06.rb
@@ -1,39 +1,3 @@
-class Lanternfish
-  def initialize(age)
-    @age = age
-  end
-
-  def grow?
-    @age -= 1
-    if @age == -1 then
-      @age = 6
-      return true
-    else
-      return false
-    end
-  end
-
-  def to_s
-    "#{@age}"  
-  end
-end
-
-def pass_day(fishes)
-  new_fish = fishes[0] || 0
-  fishes[0] = fishes[1] || 0
-  fishes[1] = fishes[2] || 0
-  fishes[2] = fishes[3] || 0
-  fishes[3] = fishes[4] || 0
-  fishes[4] = fishes[5] || 0
-  fishes[5] = fishes[6] || 0
-  fishes[6] = fishes[7] || 0
-  fishes[6] += new_fish
-  fishes[7] = fishes[8] || 0
-  fishes[8] = new_fish || 0
-  puts fishes
-end
-
-
 input = File.readlines('input.txt')[0].split(',').map(&:to_i)
 
 fishes = {}
@@ -45,10 +9,17 @@ for fish_age in input do
   end
 end
 
-puts fishes
+def pass_day(fishes)
+  new_fish = fishes[0] || 0
+  for age in 0..7 do
+    fishes[age] = fishes[age + 1] || 0
+  end
+  fishes[6] += new_fish
+  fishes[8] = new_fish || 0
+  puts fishes
+end
 
-for day in 1.. do
-  if day > 256 then break end
+for day in 1..256 do
   puts "Day #{day}:  "
   pass_day(fishes)
 end
